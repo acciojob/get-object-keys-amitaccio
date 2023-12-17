07f44cb75d7a50c131e79c82728c5ecc6899f19c
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-17 19:15:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-//your JS code here. If required.
-let student ={
-	name : "Amit", 
-	}
-student.prototype.getKeys = function () {
-	return student.keys(this);
-}
+// Creating the student object
+const student = {
+  name: 'John'
+};
+
+// Adding getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Example usage
+console.log(student.getKeys()); // Output: ['name']
